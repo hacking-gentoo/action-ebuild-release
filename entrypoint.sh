009aa058a2921beef8e5c63f382f46c7b1af7663
Max Hacking <max@hacking.co.uk>
2019-12-12 02:30:28 +0000

    Display output of kwtool.

--- entrypoint.sh
+++ entrypoint.sh
@@ -217,6 +217,7 @@ ebuild "${ebuild_file_new}" manifest --force
 
 # If no KEYWORDS are specified try to calculate the best keywords
 if [[ -z "$(unstable_keywords "${ebuild_file_new}")" ]]; then
+	kwtool b "${ebuild_cat}/${ebuild_pkg}-${ebuild_ver}"
 	new_keywords="$(kwtool b "${ebuild_cat}/${ebuild_pkg}-${ebuild_ver}")"
 	echo "Using best keywords: ${new_keywords}"
 	sed-or-die '^KEYWORDS.*' "KEYWORDS=\"${new_keywords}\"" "${ebuild_file_new}"
